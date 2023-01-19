f20032a23e05eb4b74b7d49b7f92aa94869e80fe
Alexandre L'humeau <44800463+al-humea@users.noreply.github.com>
2023-01-19 19:11:40 +0100

    just a comment deletion

--- analyze.py
+++ analyze.py
@@ -1,11 +1,3 @@
-'''
-* Fichier: analyze.py
-* Projet: automailgpt
-* Auteur: Alexandre L'HUMEAU
-* Classe: L2B
-* 01-2023 étudiant à Ascend
-'''
-
 # si pas déjà dans les brouillons cherche et feed le texte dans les mails et dans les pdf
 
 import openai
